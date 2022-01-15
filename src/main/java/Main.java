0350300c6892c13832b5178e2cf4d89f9d17b91e
yxu17 <yix.shu723@outlook.com>
2022-01-15 01:32:27 -0800

    Set up the window and its functionality (init and loop) so that it launches a window using GLFW

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import pkg1.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
---
+++ src/main/java/pkg1/Window.java
@@ -0,0 +1,78 @@
+package pkg1;
+
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private int width, height;
+    private String title;
+    private static Window window;
+    private long glfwWindow; //this is a number that is a memory address to our window
+
+    private Window(){
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "2D Game Engine";
+    }
+    public static Window get(){
+        if (Window.window == null){
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+    public void run(){
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+        init();
+        loop();
+
+    }
+    public void init(){
+        //Setup error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        //Initialize GLFW to set up our windows
+        if (!GLFW.glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        //Configure GLFW
+        GLFW.glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        //Create window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+
+        if(glfwWindow == NULL){
+            throw new IllegalStateException("Failed to create the GLFW window");
+        }
+
+        //Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        //Enable v-sync (no restriction), which is buffer swapping
+        glfwSwapInterval(1); //swaps every single frame, no wait time
+
+        //Make window visible
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities(); //makes sure we can use the bindings
+    }
+    public void loop(){
+        while (!glfwWindowShouldClose(glfwWindow)){
+            //Poll events
+            glfwPollEvents();
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT); //flushes clear color to entire screen
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
