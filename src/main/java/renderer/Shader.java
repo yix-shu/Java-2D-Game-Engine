2cb7a86e352c76d26300377c3609efa4a04c4da5
yix-shu <yix.shu723@gmail.com>
2022-01-19 04:00:36 -0800

    Setting up shader and rendering system

---
+++ src/main/java/renderer/Shader.java
@@ -0,0 +1,22 @@
+package renderer;
+
+public class Shader {
+
+    private int shaderProgramID;
+
+    public Shader(String filepath){
+
+    }
+
+    public void compile(){
+
+    }
+
+    public void use(){
+
+    }
+
+    public void detach(){
+
+    }
+}
