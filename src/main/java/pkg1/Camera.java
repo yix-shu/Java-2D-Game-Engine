39b8131dff99a5355ef13bd80912becf384cf09b
yix-shu <yix.shu723@gmail.com>
2022-01-25 18:59:09 -0800

    Setting up the projection matrix using identity matrix and ortho matrix to set the screen size and how far the projection is/how far we can see

--- src/main/java/pkg1/Camera.java
+++ src/main/java/pkg1/Camera.java
@@ -12,5 +12,11 @@ public class Camera {
         this.projectionMatrix = new Matrix4f();
         this.viewMatrix = new Matrix4f();
     }
-
+    public void adjustProjection(){
+        projectionMatrix.identity(); //creates an identity matrix
+        //creating an ortho matrix that gives us 0.0f -> 40f (32x32) units to the right and then
+        // 0.0f -> 21.0f (32x32) units to the top
+        //allows us to view objects from 0 to 100 units away (zNear and zFar)
+        projectionMatrix.ortho(0.0f, 32.0f * 40.0f, 0.0f, 32.0f*21.0f, 0.0f, 100.0f);
+    }
 }
