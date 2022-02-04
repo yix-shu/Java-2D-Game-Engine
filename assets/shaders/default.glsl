06197b85725bd24f672f287bd5f3c27325c57de0
yix-shu <yix.shu723@gmail.com>
2022-02-04 04:24:56 -0800

    Creates a black and white image with the camera moving to the bottom left

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -17,10 +17,13 @@ void main(){
 #type fragment
 #version 330 core
 
+uniform float uTime;
+
 in vec4 fColor;
 
 out vec4 color;
 
 void main(){
-    color = fColor;
+    float avg = (fColor.x + fColor.g + fColor.b) / 3;
+    color = vec4(avg, avg, avg, 1);
 }
