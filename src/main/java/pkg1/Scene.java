95958dffefa8ce48981196c6835399f2dc4245d5
yix-shu <yix.shu723@gmail.com>
2022-02-08 13:28:51 -0800

    Adding functionality of game objects in the scene (adding, starting)

--- src/main/java/pkg1/Scene.java
+++ src/main/java/pkg1/Scene.java
@@ -1,12 +1,29 @@
 package pkg1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Scene {
     protected Camera camera;
+    private boolean isRunning = false;
+    private List<GameObject> gameObjects = new ArrayList<>();
+
+
     public Scene(){
 
     }
     public void init(){
 
+    }
+    public void start(){
+        for (GameObject go: gameObjects){
+            go.start();
+        }
+    }
+    public void addGameObject(GameObject go){
+        if (!isRunning){
+            gameObjects.add(go);
+        }
     }
 
     public abstract void update(float dt);
