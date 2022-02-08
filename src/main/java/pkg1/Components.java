143c52bb78c2090b122baaaf0258c29230ecb461
yix-shu <yix.shu723@gmail.com>
2022-02-08 13:15:27 -0800

    Setting up the Entity Component System (ECS)

---
+++ src/main/java/pkg1/Components.java
@@ -0,0 +1,8 @@
+package pkg1;
+
+public abstract class Components {
+
+    public abstract void update(float dt){
+
+    }
+}
---
+++ src/main/java/pkg1/GameObject.java
@@ -0,0 +1,10 @@
+package pkg1;
+
+public class GameObject {
+    private String name;
+    private List<Component> components;
+
+    public GameObject(String name){
+        this.name = name;
+    }
+}
