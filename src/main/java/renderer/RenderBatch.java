f7eda2b7532770c47c17e20658e9e28e242c206f
yix-shu <yix.shu723@gmail.com>
2022-02-11 12:59:12 -0800

    Created RenderBatch class and method to render all sprites in an array of large batch

---
+++ src/main/java/renderer/RenderBatch.java
@@ -0,0 +1,44 @@
+package renderer;
+
+import components.SpriteRenderer;
+
+public class RenderBatch {
+    // Vertex
+    // ======
+    // Pos                        Color
+    // float, float,              float, float, float, float
+    private final int POS_SIZE = 2;
+    private final int COLOR_SIZE = 4;
+
+    private final int POS_OFFSET = 0;
+    private final int COLOR_OFFSET = POS_OFFSET + POS_SIZE * Float.BYTES; //figuring out where our color starts
+    private final int VERTEX_SIZE = 6;
+    private final int VERTEX_SIZE_BYTES = VERTEX_SIZE * Float.BYTES;
+
+
+    private SpriteRenderer[] sprites;
+    private int numSprites;
+    private boolean hasRoom;
+    private float[] vertices;
+
+    private int vaoID, vboID;
+    private int maxBatchSize;
+    private Shader shader;
+
+    public RenderBatch(int maxBatchSize){
+        shader = new Shader('assets/shaders/default.glsl');
+        shader.compile();
+        this.sprites = new SpriteRenderer[maxBatchSize];
+        this.maxBatchSize = maxBatchSize;
+
+        //4 vertices quads
+        vertices = new float[maxBatchSize * 4 * VERTEX_SIZE];
+
+        this.numSprites = 0;
+        this.hasRoom = true;
+    }
+
+    public void start(){ //create the data on the GPU
+
+    }
+}
