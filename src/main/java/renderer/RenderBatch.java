358d25dec93069520afce0bb34d0b4e6595af374
yix-shu <yix.shu723@gmail.com>
2022-02-11 13:23:34 -0800

    Created method to load color and position properties of vertices for sprite

--- src/main/java/renderer/RenderBatch.java
+++ src/main/java/renderer/RenderBatch.java
@@ -1,6 +1,7 @@
 package renderer;
 
 import components.SpriteRenderer;
+import org.joml.Vector4f;
 import pkg1.Window;
 
 import static org.lwjgl.opengl.GL15.*;
@@ -68,6 +69,20 @@ public class RenderBatch {
         glEnableVertexAttribArray(1);
     }
 
+    public void addSprite(SpriteRenderer spr){
+        // Get index and add renderObject
+        int index = this.numSprites; //adds to end of array
+        this.sprites[index] = spr;
+        this.numSprites ++;
+
+        //Add properties to local vertices array
+        loadVertexProperties(index);
+
+        if (numSprites >= this.maxBatchSize){ //filled up array of sprites
+            this.hasRoom = false;
+        }
+    }
+
     public void render(){
         //For now, rebuffer all data every frame
         glBindBuffer(GL_ARRAY_BUFFER, vboID);
@@ -90,6 +105,46 @@ public class RenderBatch {
         glBindVertexArray(0);
         shader.detach();
     }
+
+    private void loadVertexProperties(int index){
+        //Create 4 vertices per quad
+        SpriteRenderer sprite = this.sprites[index];
+
+        //Find offset within array (4 vertices per sprite)
+        int offset = index * 4 * VERTEX_SIZE;
+        //float float        float float float float float
+        Vector4f color = sprite.getColor();
+
+        //Add vertices with the appropriate properties
+
+        //*      *
+        //*      *
+
+        float xAdd = 1.0f;
+        float yAdd = 1.0f;
+        for (int i = 0; i < 4; i ++){
+            if (i == 1){
+                yAdd = 0.0f;
+            } else if (i == 2){
+                xAdd = 0.0f;
+            } else if (i == 3){
+                yAdd = 1.0f;
+            }
+
+            //Load position
+            vertices[offset] = sprite.gameObject.transform.position.x = (xAdd * sprite.gameObject.transform.scale.x);
+            vertices[offset + 1] = sprite.gameObject.transform.position.y = (yAdd * sprite.gameObject.transform.scale.y);
+
+            //Load color
+            vertices[offset + 2] = color.x;
+            vertices[offset + 3] = color.y;
+            vertices[offset + 4] = color.z;
+            vertices[offset + 5] = color.w;
+
+            offset += VERTEX_SIZE;
+        }
+    }
+
     private int[] generateIndices() {
         // 6 indices per quad (3 per triangle)
         int[] elements = new int[6 * maxBatchSize];
