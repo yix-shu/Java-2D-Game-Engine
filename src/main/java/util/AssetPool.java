209122152e98255b0db2f1635d08e0c204f813a2
yix-shu <yix.shu723@gmail.com>
2022-02-11 17:13:55 -0800

    Added texture map to store textures and created getTexture method

--- src/main/java/util/AssetPool.java
+++ src/main/java/util/AssetPool.java
@@ -1,6 +1,7 @@
 package util;
 
 import renderer.Shader;
+import renderer.Texture;
 
 import java.io.File;
 import java.util.HashMap;
@@ -8,15 +9,26 @@ import java.util.Map;
 
 public class AssetPool {
     private static Map<String, Shader> shaders = new HashMap<>();
-
+    private static Map<String, Texture> textures = new HashMap<>();
     public static Shader getShader(String resourceName){
         File file = new File(resourceName);
-        if (shaders.containsKey(file.getAbsolutePath())){
+        if (shaders.containsKey(file.getAbsolutePath())){ //checks if shader exists in asset pool
             return shaders.get(file.getAbsolutePath());
         } else{
             Shader shader = new Shader(resourceName);
             shader.compile();
-            AssetPool.shaders.put(file.getAbsolutePath(), shader);
+            AssetPool.shaders.put(file.getAbsolutePath(), shader); //adds shader into asset pool
+            return shader;
+        }
+    }
+    public static Texture getTexture (String resourceName){
+        File file = new File(resourceName);
+        if (textures.containsKey(file.getAbsolutePath())){ //checks if shader exists in asset pool
+            return textures.get(file.getAbsolutePath());
+        } else{
+            Texture texture = new Texture(resourceName);
+            AssetPool.textures.put(file.getAbsolutePath(),texture); //adds shader into asset pool
+            return texture;
         }
     }
 }
