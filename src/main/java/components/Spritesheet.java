b37af02c820744aacfd057a84f45ca8e85c79bf9
yix-shu <yix.shu723@gmail.com>
2022-02-17 20:02:10 -0800

    Calculating the texCoords of the sprites passed as spritesheet

--- src/main/java/components/Spritesheet.java
+++ src/main/java/components/Spritesheet.java
@@ -2,6 +2,7 @@ package components;
 
 import renderer.Texture;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Spritesheet {
@@ -9,7 +10,17 @@ public class Spritesheet {
     private List<Sprite> sprites;
 
     public Spritesheet(Texture texture, int spriteWidth, int spriteHeight, int numSprites, int spacing){
+        this.sprites = new ArrayList<>();
+        this.texture = texture;
+        int currentX = 0;
+        int currentY = texture.getHeight() - spriteHeight;
 
+        for (int i = 0; i < numSprites; i++){
+            float topY = 0;
+            float rightX = 0;
+            float leftX = 0;
+            float bottomY = 0;
+        }
     }
 
 
