d2fe30f640af0ad06f5010097b0868486a5714cf
yix-shu <yix.shu723@gmail.com>
2022-03-21 11:43:21 -0700

    Batching sprites based on zIndex

--- src/main/java/renderer/Renderer.java
+++ src/main/java/renderer/Renderer.java
@@ -23,7 +23,7 @@ public class Renderer {
     private void add(SpriteRenderer sprite){
         boolean added = false;
         for (RenderBatch batch : batches){
-            if (batch.hasRoom()){
+            if (batch.hasRoom() && batch.zIndex() == sprite.gameObject.getzIndex()){ //adds only sprites with the same zIndex to the same batch
                 Texture tex = sprite.getTexture();
                 if (batch.hasTexture(tex) || batch.hasTextureRoom() || tex == null){
                     batch.addSprite(sprite);
@@ -33,7 +33,7 @@ public class Renderer {
             }
         }
         if (!added) {
-            RenderBatch newBatch = new RenderBatch(MAX_BATCH_SIZE);
+            RenderBatch newBatch = new RenderBatch(MAX_BATCH_SIZE, sprite.gameObject.getzIndex());
             newBatch.start();
             batches.add(newBatch);
             newBatch.addSprite(sprite);
