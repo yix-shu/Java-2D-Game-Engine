a725939d06cbb8d7141b058d920f97bce993bb1e
yix-shu <yix.shu723@gmail.com>
2022-03-31 13:09:15 -0700

    Getting rid of constructors because gson does not accept variables with constructors

--- src/main/java/components/Sprite.java
+++ src/main/java/components/Sprite.java
@@ -4,28 +4,39 @@ import org.joml.Vector2f;
 import renderer.Texture;
 
 public class Sprite {
-    private Texture texture;
-    private Vector2f[] texCoords;
-
-    public Sprite(Texture tex){
-        this.texture = tex;
-        Vector2f[] texCoords = {
+    private Texture texture = null;
+    private Vector2f[] texCoords = {
                 new Vector2f(1, 1),
                 new Vector2f(1, 0),
                 new Vector2f(0, 0),
                 new Vector2f(0, 1),
         };
-        this.texCoords = texCoords;
-    }
-    public Sprite(Texture tex, Vector2f[] texCoords){
-        this.texture = tex;
-        this.texCoords = texCoords;
-    }
+
+//    public Sprite(Texture tex){
+//        this.texture = tex;
+//        Vector2f[] texCoords = {
+//                new Vector2f(1, 1),
+//                new Vector2f(1, 0),
+//                new Vector2f(0, 0),
+//                new Vector2f(0, 1),
+//        };
+//        this.texCoords = texCoords;
+//    }
+//    public Sprite(Texture tex, Vector2f[] texCoords){
+//        this.texture = tex;
+//        this.texCoords = texCoords;
+//    }
 
     public Texture getTexture(){
         return this.texture;
     }
     public Vector2f[] getTexCoords(){
         return this.texCoords;
+    }
+    public void setTexture(Texture tex){
+        this.texture = tex;
+    }
+    public void setTexCoords(Vector2f[] texCoords){
+        this.texCoords = texCoords;
     }
 }
--- src/main/java/components/SpriteRenderer.java
+++ src/main/java/components/SpriteRenderer.java
@@ -9,23 +9,23 @@ import renderer.Texture;
 
 public class SpriteRenderer extends Component {
 
-    private Vector4f color;
-    private Sprite sprite;
-    private boolean isDirty = false;
+    private Vector4f color = new Vector4f(1, 1, 1, 1);
+    private Sprite sprite = new Sprite();
+    private transient boolean isDirty = false;
 
-    private Transform lastTransform;
+    private transient Transform lastTransform; //transient keyword identify objects we don't want to serialize
 
-    public SpriteRenderer(Vector4f color) {
-        this.color = color;
-        this.sprite = new Sprite(null);
-        this.isDirty = true;
-    }
-
-    public SpriteRenderer(Sprite sprite){
-        this.sprite = sprite;
-        this.color = new Vector4f(1, 1, 1, 1);
-        this.isDirty = true;
-    }
+//    public SpriteRenderer(Vector4f color) {
+//        this.color = color;
+//        this.sprite = new Sprite(null);
+//        this.isDirty = true;
+//    }
+//
+//    public SpriteRenderer(Sprite sprite){
+//        this.sprite = sprite;
+//        this.color = new Vector4f(1, 1, 1, 1);
+//        this.isDirty = true;
+//    }
 
     @Override
     public void update(float dt) {
