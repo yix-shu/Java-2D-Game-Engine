1f79a859bb56c19c68338a7c46685820ec54f817
yix-shu <yix.shu723@gmail.com>
2022-04-01 19:01:46 -0700

    Exposing int variables to the level editor using imgui and created rigidbody class

--- src/main/java/pkg1/Component.java
+++ src/main/java/pkg1/Component.java
@@ -1,5 +1,9 @@
 package pkg1;
 
+import imgui.ImGui;
+
+import java.lang.reflect.Field;
+
 public abstract class Component {
 
     public transient GameObject gameObject = null;
@@ -10,6 +14,23 @@ public abstract class Component {
 
     }
     public void imgui(){
+        try{
+            Field[] fields = this.getClass().getDeclaredFields();
+            for (Field field: fields){
+                Class type = field.getType();
+                Object value = field.get(this);
+                String name = field.getName();
 
+                if (type == int.class){
+                    int val = (int) value;
+                    int[] imInt = {val};
+                    if (ImGui.dragInt(name + ": ", imInt)){ //creates a drag int for the variable
+                        field.set(this, imInt[0]);
+                    }
+                }
+            }
+        } catch(IllegalAccessException e){
+            e.printStackTrace();
+        }
     }
 }
