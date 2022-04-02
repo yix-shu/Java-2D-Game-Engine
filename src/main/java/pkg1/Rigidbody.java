1cb8918807a3c205c1e204633b3f0d4c82fa0796
yix-shu <yix.shu723@gmail.com>
2022-04-01 19:04:39 -0700

    Fixed small error: extra comma at end of Vector4f object

--- src/main/java/pkg1/Rigidbody.java
+++ src/main/java/pkg1/Rigidbody.java
@@ -7,5 +7,5 @@ public class Rigidbody extends Component{
     private int colliderType = 0;
     private float friction = 0.08f;
     public Vector3f velocity = new Vector3f(0, 0.5f, 0);
-    public transient Vector4f temp = new Vector4f(0, 0, 0,);
+    public transient Vector4f temp = new Vector4f(0, 0, 0,0);
 }
